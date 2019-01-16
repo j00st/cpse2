a01cfe3d5be8a37dbb8629ba1340072d282a67f0
Joost van Lingen <joost96nl@gmail.com>
2019-01-16 17:28:17 +0100

    Added character map

--- 4/analizer.cpp
+++ 4/analizer.cpp
@@ -6,3 +6,50 @@ analizer::analizer(std::ifstream & input){
         characters.push_back(temp);
     }
 }
+
+uint_fast32_t analizer::get_character_count(){
+    return characters.size();
+}
+
+uint_fast32_t analizer::get_line_count(){
+    return std::count_if(characters.begin(), characters.end(), [](unsigned char c){ return c == '\n'; });
+}
+
+uint_fast32_t analizer::get_alphabetic_character_count(){
+    return std::count_if(characters.begin(), characters.end(), [](unsigned char c){ return std::isalpha(c); });
+}
+
+void analizer::make_lowercase(){
+    for_each(
+        characters.begin(),
+        characters.end(),
+        [](unsigned char & c){ c = std::tolower(c); }
+    );
+}
+
+std::set<std::pair<unsigned char, uint_fast16_t>, analizer::comparator> analizer::map_characters(comparator compFunctor){
+    std::map<unsigned char, uint_fast16_t> letterMap;
+    for_each(
+        characters.begin(),
+        characters.end(),
+        [&letterMap](unsigned char & c){ if(std::isalpha(c)){ letterMap[c]++; }}
+    );
+    std::set<std::pair<unsigned char, uint_fast16_t>, comparator> letterSet(letterMap.begin(), letterMap.end(), compFunctor);
+    return letterSet;
+}
+
+void analizer::print_character_map_alphabetic(){
+    std::set<std::pair<unsigned char, uint_fast16_t>, comparator> letterSet;
+    letterSet = map_characters([](std::pair<unsigned char, uint_fast16_t> a, std::pair<unsigned char, uint_fast16_t> b){ return a.first < b.first; });
+    for(auto i : letterSet){
+        std::cout << i.first << " : " << i.second << std::endl;
+    }
+}
+
+void analizer::print_character_map_descending(){
+    std::set<std::pair<unsigned char, uint_fast16_t>, comparator> letterSet;
+    letterSet = map_characters([](std::pair<unsigned char, uint_fast16_t> a, std::pair<unsigned char, uint_fast16_t> b){ return a.second > b.second; });
+    for(auto i : letterSet){
+        std::cout << i.first << " : " << i.second << std::endl;
+    }
+}
--- 4/analizer.hpp
+++ 4/analizer.hpp
@@ -4,12 +4,24 @@
 #include <vector>
 #include <iostream>
 #include <fstream>
-
+#include <iterator>
+#include <algorithm>
+#include <map>
+#include <set>
+#include <functional>
 
 class analizer {
 public:
     analizer(std::ifstream & input);
+    uint_fast32_t get_character_count();
+    uint_fast32_t get_line_count();
+    uint_fast32_t get_alphabetic_character_count();
+    void make_lowercase();
+    void print_character_map_alphabetic();
+    void print_character_map_descending();
 private:
+    typedef std::function<bool(std::pair<unsigned char, uint_fast16_t> a, std::pair<unsigned char, uint_fast16_t> b)> comparator;
+    std::set<std::pair<unsigned char, uint_fast16_t>, comparator> map_characters(comparator compFunctor);
     std::vector<unsigned char> characters;
 };
 
